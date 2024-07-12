a60ddd459cd7835efeee40fe98473083a0bd4549
Paola Arguelles <ingpmas92@gmail.com>
2024-07-11 19:02:46 -0500

    Clase de Variables

---
+++ clase-variables.js
@@ -0,0 +1,14 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo No Permitido
+
+let c = 'Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+// Lo Permitido
+
+let primerTrasteoDeAndy = 'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '1014238782'
